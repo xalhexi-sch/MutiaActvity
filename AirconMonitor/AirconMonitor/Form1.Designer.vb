fa037a9133981b18b2917ee822f3fe423a13205e
xalhexi-sch <michael.banas@urios.edu.ph>
2025-08-14 19:52:14 +1200

    Add files via upload

---
+++ AirconMonitor/AirconMonitor/Form1.Designer.vb
@@ -0,0 +1,117 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.txtHP = New System.Windows.Forms.TextBox()
+        Me.txtType = New System.Windows.Forms.TextBox()
+        Me.btnShow = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(45, 77)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(71, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Aircon Name:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(46, 94)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(67, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Aircon Type:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(46, 117)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(71, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Horse Power:"
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(123, 70)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(100, 20)
+        Me.txtName.TabIndex = 3
+        '
+        'txtHP
+        '
+        Me.txtHP.Location = New System.Drawing.Point(123, 122)
+        Me.txtHP.Name = "txtHP"
+        Me.txtHP.Size = New System.Drawing.Size(100, 20)
+        Me.txtHP.TabIndex = 4
+        '
+        'txtType
+        '
+        Me.txtType.Location = New System.Drawing.Point(123, 96)
+        Me.txtType.Name = "txtType"
+        Me.txtType.Size = New System.Drawing.Size(100, 20)
+        Me.txtType.TabIndex = 5
+        '
+        'btnShow
+        '
+        Me.btnShow.Location = New System.Drawing.Point(123, 148)
+        Me.btnShow.Name = "btnShow"
+        Me.btnShow.Size = New System.Drawing.Size(75, 23)
+        Me.btnShow.TabIndex = 6
+        Me.btnShow.Text = "Show Details"
+        Me.btnShow.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.btnShow)
+        Me.Controls.Add(Me.txtType)
+        Me.Controls.Add(Me.txtHP)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtName As System.Windows.Forms.TextBox
+    Friend WithEvents txtHP As System.Windows.Forms.TextBox
+    Friend WithEvents txtType As System.Windows.Forms.TextBox
+    Friend WithEvents btnShow As System.Windows.Forms.Button
+
+End Class
---
+++ AirconMonitor/AirconMonitor/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+
+    Private Sub btnShow_Click(sender As System.Object, e As System.EventArgs) Handles btnShow.Click
+        ' Get values from textboxes
+        Dim name As String = txtName.Text.Trim()
+        Dim acType As String = txtType.Text.Trim()
+        Dim hp As String = txtHP.Text.Trim()
+
+        ' Validation
+        If name = "" Or acType = "" Or hp = "" Then
+            MessageBox.Show("Please fill in all fields.", "Validation", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Exit Sub
+        End If
+
+        If Not IsNumeric(hp) Then
+            MessageBox.Show("Horse Power must be a number.", "Validation", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Exit Sub
+        End If
+
+        ' Show details
+        Dim details As String = "Aircon Name: " & name & vbCrLf &
+                                "Aircon Type: " & acType & vbCrLf &
+                                "Horse Power: " & hp & " HP"
+
+        MessageBox.Show(details, "Aircon Details", MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+End Class
